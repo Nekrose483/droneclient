59f0ba028b60fe8f10644b778a4e24ea5ef19a0e
Katja DeCuir <Katja.DeCuir@gmail.com>
2012-05-21 07:11:58 +0400

    yes

---
+++ DroneClient/____Connection.cs
@@ -0,0 +1,104 @@
+﻿// This program is fully open source, although when you see "//Leave This Here" in the code.
+// Make sure to leave it.
+// Code Copyright (c) 2010 xsDevelopment & sniperX
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ChatClient
+{
+    public class tcpConnection
+    {
+        public StreamWriter Outgoing;
+        private StreamReader Incoming;
+        private TcpClient Connection;
+        private Thread Messages;
+        private IPAddress IP;
+        public string host;
+        public string nick;
+        MainChat m_ParentForm;
+        bool isConnected;
+        public tcpConnection(MainChat main, string Host, string Nickname)
+        {
+            m_ParentForm = main;
+            host = Host;
+            nick = Nickname;
+        }
+        public void Connect()
+        {
+            try
+            {
+                IP = IPAddress.Parse(host);
+                Connection = new TcpClient();
+                Connection.Connect(IP, 1986);
+                Outgoing = new StreamWriter(Connection.GetStream());
+                Outgoing.WriteLine(nick);
+                Outgoing.Flush();
+                m_ParentForm.Vis(true);
+                Messages = new Thread(new ThreadStart(this.Communication));
+                Messages.Start();
+            }
+            catch (Exception e) { m_ParentForm.Disconnected(e.Message); }
+        }
+        private void Communication()
+        {
+            Incoming = new StreamReader(Connection.GetStream());
+            string check = Incoming.ReadLine();
+            if (check[0] == '1')
+            {
+                m_ParentForm.m_parentForm.Vis(false);
+                m_ParentForm.Vis(true);
+                isConnected = true;
+            }
+            else
+            {
+                string Reason = "Disconnected: ";
+                Reason += check.Substring(2, check.Length - 2);
+                m_ParentForm.Disconnected(Reason);
+                return;
+            }
+            while (isConnected == true)
+            {
+                try
+                {
+                    ServerMessage(Incoming.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    if (isConnected == true)
+                    {
+                        m_ParentForm.Disconnected("Connection to server lost");
+                        Console.WriteLine("Connection Lost: " + e.ToString());
+                    }
+                    break;
+                }
+            }
+        }
+        private void ServerMessage(string message)
+        {
+            try
+            {
+                m_ParentForm.RecievedMessage(message);
+            }
+            catch { }
+        }
+        public void CloseConnection()
+        {
+            isConnected = false;
+            Incoming.Close();
+            Outgoing.Close();
+            Connection.Close();
+            Messages.Abort();
+        }
+        public void SendMessage(string message)
+        {
+            Outgoing.WriteLine(message);
+            Outgoing.Flush();
+        }
+    }
+}
---
+++ DroneClient/____Login.cs
@@ -0,0 +1,81 @@
+﻿// This program is fully open source, although when you see "//Leave This Here" in the code.
+// Make sure to leave it.
+// Code Copyright (c) 2010 xsDevelopment & sniperX
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class Login : Form
+    {
+        public delegate void OnError(string message);
+        public bool isConnected = false;
+        MainChat m_ChildForm;
+        public Login()
+        {
+            InitializeComponent();
+            label1.ForeColor = Color.Red;
+        }
+        public delegate void Visability(bool visable);
+        public void Vis(bool visable)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Visability(Vis), visable);
+            }
+            else
+            {
+                this.Visible = visable;
+            }
+        }
+        private void Login_Load(object sender, EventArgs e)
+        {
+            m_ChildForm = new MainChat(this);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m_ChildForm.Host = textBox1.Text;
+            m_ChildForm.Nickname = textBox2.Text;
+            m_ChildForm.StartConnection();
+        }
+        public void Disconnected(string reason)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new OnError(Disconnected), reason);
+            }
+            else
+            {
+                MessageBox.Show(reason, "Connection Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                isConnected = false;
+                this.Visible = true;
+                m_ChildForm.Visible = false;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+    }
+}
